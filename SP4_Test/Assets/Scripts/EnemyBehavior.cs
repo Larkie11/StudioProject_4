028d90b6503653cd4567f3144063597af1d7c7ab
Larkie11 <weimin.mok@gmail.com>
2017-02-15 11:45:29 +0800

    Dragon animation

--- SP4_Test/Assets/Scripts/EnemyBehavior.cs
+++ SP4_Test/Assets/Scripts/EnemyBehavior.cs
@@ -10,6 +10,27 @@ public class EnemyBehavior : MonoBehaviour
     bool moving;
     [SerializeField]
     GameObject firebreath;
+    [SerializeField]
+    int enemyType;
+
+    [SerializeField]
+    string MoveLeftAnim;
+    [SerializeField]
+    string MoveRightAnim;
+    [SerializeField]
+    float distanceToDetectPlayer;
+    [SerializeField]
+    float distanceToAttackPlayer;
+    [SerializeField]
+    int health;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float attackCD;
+    [SerializeField]
+    AnimationClip moveLeft;
+    [SerializeField]
+    AnimationClip moveRight;
 
     enum States
     {
@@ -27,7 +48,6 @@ public class EnemyBehavior : MonoBehaviour
     }
     States enemyStates;
     Attacks attack;
-    float attackCoolDown;
     // Use this for initialization
     void Start()
     {
@@ -35,77 +55,82 @@ public class EnemyBehavior : MonoBehaviour
         timer = 0;
         target = new Vector3(Random.Range(-7.5F, 7.50F), -3.7F, transform.position.z);
         timer = Random.Range(2, 5);
-        moving = true;
-        animator = GetComponent<Animator>();
-        attackCoolDown = 0;
-    }
 
-    // Update is called once per frame
-    void Update()
+        moving = false;
+        animator = GetComponent<Animator>();
+    }
+    void EnemyMovement()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         float distance = Distance(player.transform.position, transform.position);
-        
-        if(distance < 1)
+
+        if (distance < distanceToAttackPlayer)
         {
             enemyStates = States.ATTACK;
-            if(attackCoolDown <= 0F)
+            if (attackCD <= 0F && enemyType == 0)
             {
-                Attack();
-                attackCoolDown = 3F;
+                Attack(player.transform.position, Attacks.FIREBALL);
+                attackCD = 3F;
             }
-            if(player.transform.position.x > transform.position.x)
+        }
+        if (attackCD > 0 && enemyStates == States.ATTACK)
+            attackCD -= Time.deltaTime;
+
+        if (distance < distanceToDetectPlayer)
+        {
+            animator.enabled = true;
+            enemyStates = States.CHASE;
+            transform.position += (player.transform.position - transform.position).normalized * speed * Time.deltaTime;
+
+            if (player.transform.position.x > transform.position.x)
             {
-                animator.Play("GD_Right");
+                animator.Play(moveRight.name);
             }
 
             else
             {
-                animator.Play("GD_Left");
+                animator.Play(moveLeft.name);
             }
-        }
-        if (attackCoolDown > 0 && enemyStates == States.ATTACK)
-            attackCoolDown -= Time.deltaTime;
-
-        if(distance < 4)
-        {
-            enemyStates = States.CHASE;
-            transform.position += (player.transform.position - transform.position).normalized * 0.5F * Time.deltaTime;
         }
         else
         {
             if (timer >= 0)
                 timer -= Time.deltaTime;
-
-            if (timer <= 0 && enemyStates == States.IDLE)
+            if (timer <= 0)
             {
                 enemyStates = GetRandomEnum<States>();
                 Debug.Log(enemyStates);
                 timer = Random.Range(2, 5);
-            }
-            if (enemyStates == States.IDLE)
-            {
-                animator.Play("Green_Dragon");
-            }
-            if (timer <= 0 && enemyStates == States.MOVE)
-            {
-                moving = false;
-                target = new Vector3(Random.Range(-7.5F, 7.50F), -3.7F, transform.position.z);
-                
-            }if (transform.position.x < target.x)
+
+                if (enemyStates == States.IDLE)
                 {
-                    animator.Play("GD_Right");
+                    if (animator.GetCurrentAnimatorStateInfo(0).IsName("Dragon_Left"))
+                    {
+                        animator.Play("Dragon_Idle");
+                    }
+                    if (animator.GetCurrentAnimatorStateInfo(0).IsName("Dragon_Right"))
+                    {
+                        animator.Play("Dragon_IdleR");
+                    }
                 }
-                if (transform.position.x > target.x)
-                {
-                    animator.Play("GD_Left");
-                }
-            if (enemyStates == States.MOVE && !moving)
+            }
+
+            if (transform.position.x < target.x)
             {
-                transform.position += (target - transform.position).normalized * 0.5F * Time.deltaTime;
+                animator.Play(moveRight.name);
+            }
+
+            if (transform.position.x > target.x)
+            {
+                animator.Play(moveLeft.name);
+            }
+            if (enemyStates == States.MOVE)
+            {
+                transform.position += (target - transform.position).normalized * speed * Time.deltaTime;
                 timer = Random.Range(2, 5);
                 if (Distance(target, transform.position) < 1)
                 {
+                    target = new Vector3(Random.Range(-7.5F, 7.50F), -3.7F, transform.position.z);
                     moving = true;
                 }
             }
@@ -115,11 +140,26 @@ public class EnemyBehavior : MonoBehaviour
                 Debug.Log(enemyStates);
                 timer = Random.Range(5, 10);
                 moving = false;
+                
+                 if (enemyStates == States.IDLE)
+                {
+                    if (animator.GetCurrentAnimatorStateInfo(0).IsName("Dragon_Left"))
+                    {
+                        animator.Play("Dragon_Idle");
+                    }
+                    if (animator.GetCurrentAnimatorStateInfo(0).IsName("Dragon_Right"))
+                    {
+                        animator.Play("Dragon_IdleR");
+                    }
+                }
             }
         }
-        Debug.Log(distance);
-        
-        int toNextState = Random.Range(2, 5);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMovement();
     }
 
     private float Distance(Vector3 pos1, Vector3 pos2)
@@ -136,8 +176,15 @@ public class EnemyBehavior : MonoBehaviour
     {
 
     }
-    void Attack()
+    void Attack(Vector2 playerPos, Attacks a)
     {
-        GameObject go = Instantiate(firebreath, new Vector3(transform.position.x,transform.position.y,transform.position.z-3), Quaternion.identity) as GameObject;
+        GameObject go;
+        if (a == Attacks.FIREBALL)
+        {
+            if (transform.position.x < playerPos.x)
+                go = Instantiate(firebreath, new Vector3(transform.position.x, transform.position.y, transform.position.z - 3), Quaternion.Euler(0, 90, 0)) as GameObject;
+            else
+                go = Instantiate(firebreath, new Vector3(transform.position.x, transform.position.y, transform.position.z - 3), Quaternion.Euler(0, -90, 0)) as GameObject;
+        }
     }
 }
