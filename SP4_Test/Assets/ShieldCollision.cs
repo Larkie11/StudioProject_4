2fa6905a2548ad782249909800d8b7543cf05b33
Larkie11 <weimin.mok@gmail.com>
2017-02-17 13:21:12 +0800

    Player shield fix for collision

--- SP4_Test/Assets/ShieldCollision.cs
+++ SP4_Test/Assets/ShieldCollision.cs
@@ -8,16 +8,10 @@ public class ShieldCollision : MonoBehaviour {
     void Start () {
 
 
-        Debug.Log(platformPref.name);
-
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.transform.tag == "Platform")
-        {
-            Physics2D.IgnoreCollision(collision.collider, transform.GetComponent<Collider2D>());
-            Debug.Log("AAA");
-        }
+       
     }
     // Update is called once per frame
     void Update () {
