1a2ed9c6cc47301dc5590a6f6b08fcaf34d68193
Larkie11 <weimin.mok@gmail.com>
2017-02-17 12:10:49 +0800

    a

--- SP4_Test/Assets/ShieldCollision.cs
+++ SP4_Test/Assets/ShieldCollision.cs
@@ -3,15 +3,13 @@ using System.Collections;
 
 public class ShieldCollision : MonoBehaviour {
 
-    public GameObject platformPref;
 
     // Use this for initialization
     void Start () {
 
-        platformPref = GameObject.FindGameObjectWithTag("Platform");
 
-        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), platformPref.GetComponent<Collider2D>(), true);
         Debug.Log(platformPref.name);
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
